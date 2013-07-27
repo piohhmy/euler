ecd4a5b118e706dc104fd18bd834af6f93c3e657
Danny Varner <danny.varner@gmail.com>
2013-07-26 20:35:17 -0700

    Memoizing recursive function

--- p2.py
+++ p2.py
@@ -1,72 +1,71 @@
-# 1, 2, 3, 5, 8, 13,
-# 1, 2, 3, 4, 5, 6
-
 import itertools
+from timeit import Timer
+
+def memoize(func):
+    memoized_results = {}
+    def wrapper(*args):
+        if memoized_results.has_key(args):
+            return memoized_results[args]
+        else:
+            result = func(*args)
+            memoized_results[args] = result
+            return result
+    return wrapper
 
 
-def fib(i):
-    """ Finds fibinocci recursively for any given index """
+
+@memoize
+def fib_finder(i):
+    """ Finds fibonacci recursively for any given index """
     if i == 1: return 1
     if i == 2: return 2
-    return fib(i-1) + fib(i-2)
+    return fib_finder(i-1) + fib_finder(i-2)
 
-def fib_gen():
-    """ Iterative generator for all fib numbers """
-    yield 1
-    yield 2
-    last2 = 1
-    last = 2 
-    for i in itertools.count(1):
-        result = last + last2
-        yield result
-        last2 = last
-        last = result
 
-def fib_gen_recursive(prev=2, prev2=1):
+
+def fib_generator():
+    """ Iterative generator for all fibonacci numbers """
+    last = 1
+    curr = 2 
+    yield last
+    yield curr
+
+    while True:
+        next = curr + last
+        yield next
+        last = curr
+        curr = next
+
+
+def fib_generator_recursive(curr=2, prev=1):
     """ Recursive generator for all fib numbers """
-    if prev == 2:
+    if curr == 2:
         yield 1
         yield 2
-    result = prev + prev2
-    yield result
-    for val in fib_gen_recursive(prev=result, prev2=prev):
+    next = curr + prev
+    yield next
+    for val in fib_generator_recursive(next, curr):
         yield val
-    
 
 
+def fibs_less_than(max):
+    return itertools.takewhile(lambda x: x < max, itertools.imap(fib_finder, itertools.count(1)))
 
-def iterative_solver():
-    return sum(filter(iseven, itertools.takewhile(lambda x: x < 4000000, fib_gen())))
+def p2_recursive():
+    return sum(filter(lambda x: x % 2 == 0, fibs_less_than(4000000)))
 
-def recurrsive_solver():
-    return sum(filter(iseven, itertools.takewhile(lambda x: x < 4000000, fib_gen_recursive())))    
+def p2_iterative_generator():
+    return sum(filter(lambda x: x % 2 == 0, itertools.takewhile(lambda x: x < 4000000, fib_generator())))  
 
-def test_fib_max():
-    assert 1, fib_max(1)
-    assert 2, fib_max(2)
-    assert 3, fib_max(3)
-    assert 3, fib_max(4)
-    assert 5, fib_max(5)
+def p2_recursive_generator():
+    return sum(filter(lambda x: x % 2 == 0, itertools.takewhile(lambda x: x < 4000000, fib_generator_recursive())))  
 
-def iseven(x):
-    return x % 2 == 0   
-
-def fibs_that_calc_less_than(max):
-    return itertools.takewhile(lambda x: x < max, itertools.imap(fib, itertools.count(1)))
-
-def functional_solver():
-    return sum(filter(iseven, fibs_that_calc_less_than(4000000)))
-
-
-def optimized_functional_solver():
-    return sum(filter(iseven, fib_max(1, 4000000)))
-
-def primitve_loop_solver():
+def p2_primitve():
     x = 1
     total = 0
 
     while(True):
-        result = fib(x)
+        result = fib_finder(x)
         if result > 4000000:
             break
         if result % 2 == 0:
@@ -75,19 +74,11 @@ def primitve_loop_solver():
     return total
 
 
-from timeit import Timer
 
 def time_impls():
-    solvers = [recurrsive_solver, iterative_solver]
+    runs = 1000
+    solvers = [p2_recursive, p2_iterative_generator, p2_recursive_generator, p2_primitve]
     for solver in solvers:
         t = Timer(solver)
-        print t.timeit(number=100)
-
-def test_primitive_solver():
-    assert primitve_loop_solver(), 4613732
-
-def test_functional_solver():
-    assert functional_solver(), 4613732
-
-
-
+        assert solver(), 4613732
+        print "%d runs of %s solved in %f seconds" % (runs, solver.__name__, t.timeit(number=1000))
