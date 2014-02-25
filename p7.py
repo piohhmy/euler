5be97fe393ffb405d58576c0e4267be73a9b0adb
Danny Varner <danny.varner@gmail.com>
2014-02-25 00:40:53 -0800

    reducing size of p7 functions

--- p7.py
+++ p7.py
@@ -2,15 +2,9 @@ import unittest
 import itertools
 import math
 
-class TestP7(unittest.TestCase):
-	def test_6th_prime(self):
-		self.assertEqual(13, calc_prime(6))
 
 def is_prime(num):
-	for x in xrange(2, int(math.sqrt(num)+1)):
-		if num % x == 0: 
-			return False	
-	return True
+    return all(num % x != 0 for x in xrange(2, int(math.sqrt(num)+1)))
 
 def prime_generator():
     return (num for num in itertools.count(2) if is_prime(num))
@@ -18,6 +12,10 @@ def prime_generator():
 def calc_prime(num):
 	return next(itertools.islice(prime_generator(), num-1, None))
 
+
+class TestP7(unittest.TestCase):
+    def test_6th_prime(self):
+        self.assertEqual(13, calc_prime(6))
 if __name__ == '__main__':
 	import time
 	start = time.time()
