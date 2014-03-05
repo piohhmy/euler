c0db88e4e6f765efbf9b563036189ddb5c1bccaa
Danny Varner <danny.varner@gmail.com>
2014-03-04 21:44:20 -0800

    solving p9

---
+++ v9.py
@@ -0,0 +1,13 @@
+import math
+
+def solve_p9():
+    for a in range(1,500):
+        for b in range(a+1,1001):
+            c = math.sqrt(a**2 + b**2)
+            if (a + b + c == 1000) and (a**2 + b**2 == c**2):
+                return a*b*c
+
+
+if __name__ == '__main__':
+    print solve_p9()
+
