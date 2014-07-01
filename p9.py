a7ff95fcc8d6e5fa8d90c369ce159367f54b29e0
Danny Varner <danny.varner@intel.com>
2014-07-01 10:20:14 -0700

    Reducing size of loop in p9

--- p9.py
+++ p9.py
@@ -2,7 +2,7 @@ import math
 
 def solve_p9():
     for a in range(1,500):
-        for b in range(a+1,1001):
+        for b in range(a+1,500):
             c = math.sqrt(a**2 + b**2)
             if (a + b + c == 1000) and (a**2 + b**2 == c**2):
                 return a*b*c
