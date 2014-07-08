b9b3a09d00624375d4bc870cffdfc33aa203c4e8
Danny Varner <danny.varner@gmail.com>
2014-07-07 22:32:15 -0700

    Fixing issue w/ p10

--- p10.py
+++ p10.py
@@ -1,12 +1,12 @@
 import math
 def is_prime(x):
-    for y in range(2,int(math.ceil(math.sqrt(x)))):
+    for y in range(2,int(math.sqrt(x)) + 1):
         if x % y == 0:
             return False
     return True
 
 def solve_p10():
-    return sum([x for x in xrange(2000000) if is_prime(x)])
+    return sum([x for x in xrange(2, 2000000) if is_prime(x)])
 
 if __name__ == '__main__':
     print solve_p10()
