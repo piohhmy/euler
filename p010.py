fde01dd8ab62504c1d1bb59b13b75173ec39c75a
Danny Varner <danny.varner@gmail.com>
2014-07-14 22:28:05 -0700

    Adding a generator approach to p10

--- p010.py
+++ p010.py
@@ -5,6 +5,9 @@ from timeit import Timer
 def solve_p10_sieve():
     return sum(sieve_primes_up_to(2000000))
 
+def solve_p10_sieve_w_generator():
+    return sum(prime_sieve_generator(2000000))
+
 def sieve_primes_up_to(num):
     num_list = range(num)
     num_list[1] = 0 # cross out 1, special case
@@ -18,7 +21,21 @@ def sieve_primes_up_to(num):
 
 def cross_out_composite_multiples(multiple, num_list):
     for composite in xrange(multiple*2, len(num_list), multiple):
-        num_list[composite] = 0 
+        num_list[composite] = 0
+
+def prime_sieve_generator(limit):
+    num_list = [True] * limit
+    num_list[0] = num_list[1] = False
+    square_root_limit = int(math.sqrt(limit))+1
+    for i, isprime in enumerate(num_list):
+        if isprime:
+            yield i
+            if i > square_root_limit:
+               continue #optimization, composites already marked
+            for composite in xrange(i*2, len(num_list), i):
+                num_list[composite] = False
+
+
 
 def next_non_crossed_out_index(i, num_list):
     increment_index = True
@@ -29,7 +46,10 @@ def next_non_crossed_out_index(i, num_list):
 
 if __name__ == '__main__':
     sieve_timer = Timer(solve_p10_sieve)
-    print sieve_timer.timeit(1)
+    sieve_generator_timer = Timer(solve_p10_sieve_w_generator)
+    print "Filter approach: {0}".format(sieve_timer.timeit(1))
+    print "Generator approach: {0}".format(sieve_generator_timer.timeit(1))
 
     print solve_p10_sieve()
+    print solve_p10_sieve_w_generator()
 
