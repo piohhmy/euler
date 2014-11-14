dad8cc72a0553f16edbe31a357e90b30fe342e0e
Danny Varner <danny.varner@intel.com>
2014-11-13 23:02:57 -0800

    fixing typo

--- p025.py
+++ p025.py
@@ -5,16 +5,15 @@ What is the first term in the Fibonacci sequence to contain 1000 digits?
 def solve_p025():
     for i, num in enumerate(fib_generator()):
         if len(str(num)) == 1000:
-            return i + i
+            return i + 1
 
 def fib_generator():
     prev = 1
     curr = 1
     yield prev
-    yield curr
     while True:
-        prev, curr = curr, prev + curr
         yield curr
+        prev, curr = curr, prev + curr
 
 if __name__ == '__main__':
     print solve_p025()
