0379e0d1647efd2f1a9a305732fd682559daf0d9
Danny Varner <danny.varner@gmail.com>
2014-12-31 12:21:53 -0800

    Solving p31

---
+++ p031.py
@@ -0,0 +1,17 @@
+def change_combos(coins, change):
+    if change == 0:
+        return 1
+    if change < 0:
+        return 0
+    if change >=1 and len(coins) <= 0:
+        return 0
+
+    # try solving with one less coin and try solving with current coins
+    return change_combos(coins[:-1], change) + change_combos(coins, change - coins[-1])
+
+def solve_p030():
+    coins = [1,2,5,10,20,50,100,200]
+    return count_combos(coins, 200)
+
+if __name__ == '__main__':
+    print solve_p030()
