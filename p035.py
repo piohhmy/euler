0c306dc55523b82afa40be2502e024e22b602c37
Danny Varner <danny.varner@intel.com>
2015-01-05 18:52:19 -0800

    Solving p035

---
+++ p035.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def is_prime(num):
+    for i in xrange(2, int(num**.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def is_circular_prime(num):
+    num = deque(str(num))
+    
+    for _ in xrange(len(num)):
+        if not is_prime(int("".join(num))):
+            return False
+        num.rotate(1)
+    return True
+
+
+def solve_p35():
+    return len([num for num in xrange(2, 1000000) if is_circular_prime(num)])
+
+if __name__ == '__main__':
+    print solve_p35()
+    
+
