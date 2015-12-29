f25b2408f2a36d0e4c9b022155d05ed1fd72e58f
Danny Varner <danny@sellerengine.com>
2015-12-28 22:06:11 -0800

    Solve p040 faster

--- p040.py
+++ p040.py
@@ -1,4 +1,22 @@
 from itertools import *
+from functools import reduce
+import math
+import operator
+
+
+def find_champernowne_for(digit):
+    c = 0
+    counter = count(1)
+    while c<digit:
+        n = next(counter)
+        length = (int(math.log10(n))+1)
+        c += length
+    index = length-(c-digit)-1
+    return int(str(n)[index])
+
+def solve_p040():
+    numbers = [find_champernowne_for(x) for x in (1,10,100,1000,10000,100000)]
+    return reduce(operator.mul, numbers)
 
 def build_champernowne():
     c = ''
@@ -13,11 +31,8 @@ def build_up_to(builder, limit):
         if len(a) > limit:
             return a
 
-
-def solve_p040():
+def solve_p040_slow():
     builder = build_champernowne()
     a = build_up_to(builder, 1000000)
     return int(a[0]) * int(a[9]) * int(a[99]) * int(a[999]) * int(a[9999]) * int(a[99999]) * int(a[999999])
-
-
 
